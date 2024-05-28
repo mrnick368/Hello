91976011c60f33b63053066ccbd89f24a898a70f
mrnick368 <mrnick368@gmail.com>
2024-05-28 09:48:14 +0530

    updated Hello from B-2

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 import java.utils.*;
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello World from B-1");
+		System.out.println("Hello World from B-2);
 	}
 }
